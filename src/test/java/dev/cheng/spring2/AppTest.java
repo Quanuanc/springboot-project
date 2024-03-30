8abefeacf1af5aacd42de6ad013ba61c8956c41e
Quanuanc <quanuanc@outlook.com>
2024-03-30 12:45:53 +0800

    initial project

---
+++ src/test/java/dev/cheng/spring2/AppTest.java
@@ -0,0 +1,13 @@
+package dev.cheng.spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void test1() {
+    }
+
+}
