f895d76ef57b78a6387a828af261dd2f8b53079e
Quanuanc <quanuanc@outlook.com>
2024-03-30 13:32:01 +0800

    spring boot 3.0

--- src/main/java/dev/cheng/spring/App.java
+++ src/main/java/dev/cheng/spring/App.java
@@ -1,4 +1,4 @@
-package dev.cheng.spring2;
+package dev.cheng.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/cheng/spring/AppTest.java
+++ src/test/java/dev/cheng/spring/AppTest.java
@@ -1,4 +1,4 @@
-package dev.cheng.spring2;
+package dev.cheng.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
